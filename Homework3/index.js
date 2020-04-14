332292e13dc13f9f734333147c36779b670a2340
china555 <kittikorn.kee@student.mahidol.edu>
2020-04-14 12:20:19 +0700

    Final Version

--- Homework3/index.js
+++ Homework3/index.js
@@ -1,6 +1,8 @@
+// API Key
 const API_KEY = "AIzaSyDLRdnFCYofzUQtdoZ9rUoPWRqVq8PgjAs";
 
-function videoCard(title, videoId, thumbnails) {
+//for create card use for html page
+function videoCard(title, videoId, thumbnails) { // call function to return back the html tag to append
     return `
     <div class="card">
       <p>Title: ${title}</p>
@@ -13,17 +15,21 @@ function videoCard(title, videoId, thumbnails) {
  * call GET request to Youtube Data API
  */
 function callYouTubeAPI(value) {
-    $('#loading').show();
-    const URL = `https://content.googleapis.com/youtube/v3/search?part=snippet&maxResults=21&q=${value}&type=video&key=${API_KEY}`;
-    $.get(URL, function ({items}, status) {
+    $('#loading').show(); // show loading animation
+    const URL = `https://content.googleapis.com/youtube/v3/search?part=snippet&maxResults=21&q=${value}&type=video&key=${API_KEY}`; // url api
+    console.log(URL)
+    $.get(URL, function ({
+        items
+    }, status) { // get item back from that link it will be object arry
         // iterate each element to render
-        items.map(function (value) {
-            $("#list-videos").append(
+        console.log(items.length)
+        items.map(function (value) { // map function
+            $("#list-videos").append( //to append 
                 videoCard(
                     value.snippet.title,
                     value.id.videoId,
                     value.snippet.thumbnails
-                )
+                ) //call function and pass all variable
             );
         });
         // stop loading animation
@@ -31,12 +37,12 @@ function callYouTubeAPI(value) {
     });
 }
 
-$(document).ready(function () {
-    $('#loading').hide();
+$(document).ready(function () { // jquery ready to begin
+    $('#loading').hide(); // loading section to hide when we didn't get any video
 
-    $('#search').click(function () {
-        const value = $("input").val();
-        $("#list-videos").empty();
-        callYouTubeAPI(value);
+    $('#search').click(function () { //when we have click action in button that have search id it will do in this one
+        const value = $("input").val(); // get user input value 
+        $("#list-videos").empty(); // To refresh every time when search
+        callYouTubeAPI(value); // call function
     });
 })
