77587b7fac56b1cc56bddfc91bc024ae7c853fed
china555 <kittikorn.kee@student.mahidol.edu>
2020-04-23 02:54:35 +0700

    V2

--- lab/week14-node-js/StudentService.js
+++ lab/week14-node-js/StudentService.js
@@ -3,12 +3,14 @@ const mysql = require('mysql2/promise')
 const dotenv = require('dotenv');
 const route = express.Router();
 const app = express();
+const bodyParse = require('body-parser');
 //connect to database
 dotenv.config()
 
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(bodyParse.json())
 
 app.use('/', route);
 //Non Persistant 
@@ -19,90 +21,91 @@ const connection = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 })
 
-route.post('/student', function (req, res) {
+route.post('/student', async (req, res) => {
     let student = req.body.student;
-    console.log(student);
+    // console.log(req.body);
     if (!student) {
         return res.status(400).send({
             error: true,
             message: 'Please provide student'
         });
     }
-    connection.query(`INSERT INTO student SET StudentID = ${student.STU_ID},Firstname = ${student.STU_FNAME}`, student, function (error, results) {
-        if (error) throw error;
-        return res.send({
-            error: false,
-            data: results.affectedRows,
-            message: 'New student has been created successfully.'
-        });
+    student.Phone = "0123456789"
+    student.DOB = `${2020 - Number(student.DOB)}-01-01`
+    // console.log(student);
+    const sql = `INSERT INTO personal_info SET ?`
+    // console.log(sql)
+    const affect = await connection.query(sql, student)
+    return res.send({
+        error: false,
+        data: affect.affectedRows,
+        message: 'New student has been created successfully.'
     });
 });
 
-route.put('/student', function (req, res) {
-    let student_id = req.body.student_id;
+route.put('/student', async (req, res) => {
+    let student_id = req.body.StudentID;
     let student = req.body.student;
+    // console.log(student_id)
     if (!student_id || !student) {
         return res.status(400).send({
             error: student,
             message: 'Please provide student and student_id '
         });
     }
-    connection.query("UPDATE student SET ? WHERE STU_ID = ?", [student, student_id], function (error, results) {
-        if (error) throw error;
-        return res.send({
-            error: false,
-            data: results.affectedRows,
-            message: 'Student has been updated successfully.'
-        })
-    });
+    student.Phone = "0123456789"
+    student.DOB = `${2020 - Number(student.DOB)}-01-01`
+    const sql = 'UPDATE personal_info SET ? WHERE StudentID = ?';
+    const affect = await connection.query(sql, [student, student_id])
+    return res.send({
+        error: false,
+        data: affect.affectedRows,
+        message: 'Student has been updated successfully.'
+    })
 });
 
-route.delete('/student', function (req, res) {
-    let student_id = req.body.student_id;
+route.delete('/student', async (req, res) => {
+    let student_id = req.body.StudentID;
     if (!student_id) {
         return res.status(400).send({
             error: true,
             message: 'Please provide student_id'
         });
     }
-    connection.query('DELETE FROM personal_info WHERE STU_ID = ?', [student_id], function (error, results) {
-        if (error) throw error;
-        return res.send({
-            error: false,
-            data: results.affectedRows,
-            message: 'Student has been deleted successfully.'
-        });
+    const sql = 'DELETE FROM personal_info WHERE StudentID = ?'
+    connection.query(sql, [student_id])
+    return res.send({
+        error: false,
+        message: 'Student has been deleted successfully.'
     });
 });
 
-route.get('/student/:id', function (req, res) {
+route.get('/student/:id', async (req, res) =>{
     let student_id = req.params.id;
+    console.log(student_id)
     if (!student_id) {
         return res.status(400).send({
             error: true,
             message: 'Please provide student id.'
         });
     }
-    connection.query('SELECT * FROM personal_info where STU_ID=?', student_id, function (error, results) {
-        if (error) throw error;
+    const sql = 'SELECT * FROM personal_info where StudentID=?'
+    const [data] = await connection.query(sql, student_id,) 
         return res.send({
             error: false,
-            data: results[0],
+            data: data,
             message: 'Student retrieved'
         });
-    });
 });
 
 
-route.get('/students', function (req, res) {
-    connection.query('SELECT * FROM personal_info', function (error, results) {
-        if (error) throw error;
+route.get('/students', async (req, res) => {
+    const sql = 'SELECT * FROM personal_info'
+    await connection.query(sql)
         return res.send({
             error: false,
-            data: results,
             message: 'Student list.'
         });
-    });
 });
 
 app.listen(process.env.PORT)
